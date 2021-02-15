8ae09cbd4797eb2f28fa73ae37df3b1dca07dba8
Diego Celis <diegofercelis@gmail.com>
2021-02-15 01:17:23 -0300

    Definição das primeiras variáveis

---
+++ script.js
@@ -0,0 +1,6 @@
+let cor= prompt ("Escreva uma cor")
+let marca = prompt ("Escreva uma marca de carro que você goste")
+let número = prompt ("Escreva um número qualquer")
+Number(número)
+número=número*210
+console.log (marca, cor, número)

52c51dab71fc9ad3d80104daa6579672637c230b
Diego Celis <diegofercelis@gmail.com>
2021-02-16 01:13:45 -0300

    Melhoria da sintaxe

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let cor= prompt ("Escreva uma cor")
-let marca = prompt ("Escreva uma marca de carro ou moto que você goste")
-let número = prompt ("Escreva um número qualquer")
-let gênero = prompt ("Por último, escreva \"h\" se você é homem, ou \"m\" se é mulher")
-Number(número)
-let baterias = número/7
-let capacitor = número/3
-número = número*210
+let cor = prompt ("Escreva uma cor");
+let marca = prompt ("Escreva uma marca de carro ou moto que você goste");
+let número = prompt ("Escreva um número qualquer");
+let gênero = prompt ("Por último, escreva \"h\" se você é homem, ou \"m\" se é mulher");
+Number(número);
+let baterias = número/7;
+let capacitor = número/3;
+número = número*210;
 if (gênero=="m"){
-    console.log ("É o Ano 2077. A humanidade está em guerra com as máquinas.\n\nA sua missão, caso decida aceitar, será infiltrar-se na base andróide do MK ULTRA 9600, fingir ser uma robô e coletar informações.\n\nSeu novo nome como espiã robótica será:\n\n","A", "poderosa", marca, Number(2077),"-",número, "do chassis", cor,)
-    console.log ("")
-    console.log ("As suas especificações técnicas são as seguintes:")
+    console.log ("É o Ano 2077. A humanidade está em guerra com as máquinas.\n\nA sua missão, caso decida aceitar, será infiltrar-se na base andróide do MK ULTRA 9600, fingir ser uma robô e coletar informações.\n\nSeu novo nome como espiã robótica será:\n\n","A", "poderosa", marca, Number(2077),"-",número, "do chassis", cor,);
+    console.log ("");
+    console.log ("As suas especificações técnicas serão as seguintes:");
     console.table (
         {  
             "Modelo": `2077-${número}`,
@@ -22,9 +22,9 @@ if (gênero=="m"){
     )
 }
 else{
-    console.log ("É o Ano 2077. A humanidade está em guerra com as máquinas.\n\nSua missão, caso queira aceitar, será infiltrar-se na base andróide do MK ULTRA 9600, fingir ser um robô e coletar informações.\n\nSeu novo nome como espião robótico será:\n\n","O", "poderoso", marca, Number(2077),"-",número, "do chassis", cor,)
-    console.log ("")
-    console.log ("As suas especificações técnicas são as seguintes:")
+    console.log ("É o Ano 2077. A humanidade está em guerra com as máquinas.\n\nSua missão, caso queira aceitar, será infiltrar-se na base andróide do MK ULTRA 9600, fingir ser um robô e coletar informações.\n\nSeu novo nome como espião robótico será:\n\n","O", "poderoso", marca, Number(2077),"-",número, "do chassis", cor,);
+    console.log ("");
+    console.log ("As suas especificações técnicas serão as seguintes:");
     console.table (
         {
             "Modelo": `2077-${número}`,
